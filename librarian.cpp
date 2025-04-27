2fc1b7e3efc6effa4d8998ebd110013a4933bc1c
Radwa-Ayman239 <radwaayman@aucegypt.edu>
2025-04-28 02:33:42 +0300

    Delete librarian.cpp

--- librarian.cpp
+++
@@ -1,9 +0,0 @@
-#include "librarian.h"
-
-Librarian::Librarian(const QString& username, const QString& password)
-    : LibraryMember(username, password) {}
-
-QString Librarian::getRole() const {
-    return "librarian";
-}
-
